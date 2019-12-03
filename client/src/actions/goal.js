81ea13c682829f62114bffacd05c0f1927a905a7
k37singh <k37singh@gmail.com>
2019-12-02 21:50:44 -0500

    upsertTitle

--- client/src/actions/goal.js
+++ client/src/actions/goal.js
@@ -1,12 +1,16 @@
 import axios from 'axios';
 
 import { 
+    ADD_GOAL_REQUEST,
+    ADD_GOAL_RESPONSE,
     GET_GOAL_REQUEST,
     GET_GOAL_RESPONSE,
+    UPDATE_GOAL_REQUEST,
+    UPDATE_GOAL_RESPONSE
 } from './types';
 
 export const getGoal = () => {
-    return async (dispatch) => {
+    return async dispatch => {
         dispatch({ type: GET_GOAL_REQUEST })
         let response
         try {
@@ -17,5 +21,34 @@ export const getGoal = () => {
             console.log(e)
             dispatch({ type: GET_GOAL_RESPONSE, goal: null })
         }
+    }
+}
+
+export const upsertGoal = goalFields => {
+    return async (dispatch, getState) => {
+        let response
+        const { goal: { _id = null } = {} } = getState()
+        if(_id) {
+            dispatch({ type: UPDATE_GOAL_REQUEST})
+            try {
+                response = await axios.post(`/goal/update`, { _id, ...goalFields })
+                const { updateGoal } = response
+                dispatch({ type: UPDATE_GOAL_RESPONSE, success: updateGoal})
+            } catch (e) {
+                console.log(e)
+                dispatch({ type: UPDATE_GOAL_RESPONSE, success: null })
+            }
+        } else {
+            dispatch({ type: ADD_GOAL_REQUEST})
+            try {
+                response = await axios.post(`/goal/`, goalFields)
+                const { addGoal } = response
+                dispatch({ type: ADD_GOAL_RESPONSE, success: addGoal})
+            } catch (e) {
+                console.log(e)
+                dispatch({ type: ADD_GOAL_RESPONSE, success: null })
+            }
+        }
+        
     }
 }
--- client/src/components/titleInput.js
+++ client/src/components/titleInput.js
@@ -1,25 +1,34 @@
 import React, { useState, useEffect } from 'react'
+import { connect } from 'react-redux'
 import { Input } from 'antd'
 
+import { upsertGoal } from '../actions'
 
-export default ({ title }) => {
-    console.log({title})
+export const TitleInput = ({ title, ...actions }) => {
     const [value, setValue] = useState(`${title}`)
-    console.log({value})
-    
-    const className = "titleInput"
-    const placeHolder = "Please enter your Goal Here"
 
-    const onChange = (e) => {
+    useEffect(() => {
+        setValue(title)
+    }, [title])
+
+    const onChange = e => {
         e.preventDefault()
         setValue(e.target.value)
     }
+
+    const onSubmit = e => {
+        e.preventDefault()
+        actions.upsertGoal({title:value})
+    }
     return (
         <Input
-            className={className}
+            className={"titleInput"}
             value={value}
-            placeholder= {placeHolder}
+            placeholder={"Please enter your Goal Here"}
             onChange={onChange}
+            onPressEnter={onSubmit}
         />
     );
 }
+
+export default connect(null, { upsertGoal })(TitleInput)
